6a2efa4b4b611da01676b46f42730951b2c97260
Sebastiano Guerriero <se.guerriero@gmail.com>
2018-12-13 10:19:09 +0000

    updated indentation
    from 4 spaces to 2

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,26 @@ var cssvariables = require('postcss-css-variables');
 var calc = require('postcss-calc');  
 
 gulp.task('sass', function() {
-  	return gulp.src('main/assets/css/**/*.scss')
-    	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    	.pipe(postcss([autoprefixer(), cssvariables({preserve: true}), calc()]))
-    	.pipe(gulp.dest('main/assets/css'))
-    	.pipe(browserSync.reload({
-      		stream: true
-    	}));
+  return gulp.src('main/assets/css/**/*.scss')
+  .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+  .pipe(postcss([autoprefixer(), cssvariables({preserve: true}), calc()]))
+  .pipe(gulp.dest('main/assets/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }));
 });
 
 gulp.task('browserSync', function() {
-  	browserSync.init({
-    	server: {
-      	baseDir: 'main'
-    	},
-  	})
+  browserSync.init({
+    server: {
+      baseDir: 'main'
+    },
+  })
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
-  	gulp.watch('main/assets/css/**/*.scss', ['sass']);
-    gulp.watch('main/*.html', browserSync.reload);
-    gulp.watch('main/demo/*.html', browserSync.reload); 
-    gulp.watch('main/assets/js/**/*.js', browserSync.reload);
+  gulp.watch('main/assets/css/**/*.scss', ['sass']);
+  gulp.watch('main/*.html', browserSync.reload);
+  gulp.watch('main/demo/*.html', browserSync.reload); 
+  gulp.watch('main/assets/js/**/*.js', browserSync.reload);
 });
