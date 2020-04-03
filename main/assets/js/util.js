362a9c51f5138a36316b7b58d31654ed7c5118bb
claudia-romano <claudia.romano06@gmail.com>
2020-04-03 15:27:01 +0200

    New backdrop util classes + easing util functions
    - Added .backdrop-blur utility classes;
    - Added .bg-no-repeat util class;
    - In util.js file: added quartic easing functions

--- main/assets/js/util.js
+++ main/assets/js/util.js
@@ -240,6 +240,24 @@ Math.easeInOutQuad = function (t, b, c, d) {
 	return -c/2 * (t*(t-2) - 1) + b;
 };
 
+Math.easeInQuart = function (t, b, c, d) {
+	t /= d;
+	return c*t*t*t*t + b;
+};
+
+Math.easeOutQuart = function (t, b, c, d) { 
+  t /= d;
+	t--;
+	return -c * (t*t*t*t - 1) + b;
+};
+
+Math.easeInOutQuart = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t*t + b;
+	t -= 2;
+	return -c/2 * (t*t*t*t - 2) + b;
+};
+
 
 /* JS Utility Classes */
 (function() {
