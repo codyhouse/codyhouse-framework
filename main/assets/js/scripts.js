5c3d7ff227a2b28f37a24327aaac509bf03a88d7
claudia-romano <claudia.romano06@gmail.com>
2020-04-15 11:47:36 +0200

    New util classes + elastic easing function
    - Added .text-xxxxl and .text-xxxxl@{breakpoint} util classes;
    - Added .momentum-scrolling and .overscroll-contain util classes;
    - Adding custom breakpoints will now modify the responsive helper classes;
    - Added new JS  elastic easing function.

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -258,6 +258,14 @@ Math.easeInOutQuart = function (t, b, c, d) {
 	return -c/2 * (t*t*t*t - 2) + b;
 };
 
+Math.easeOutElastic = function (t, b, c, d) {
+  var s=1.70158;var p=d*0.7;var a=c;
+  if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+  if (a < Math.abs(c)) { a=c; var s=p/4; }
+  else var s = p/(2*Math.PI) * Math.asin (c/a);
+  return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+};
+
 
 /* JS Utility Classes */
 (function() {
